f6f6269d87ef8734b867b0109cbf5992f4ea9a65
Ihor Nechyporenko <nechyporenko.i@gmail.com>
2020-12-27 03:33:27 +0200

    tasks 1-6

---
+++ js/task-2.js
@@ -0,0 +1,11 @@
+// Базовый код
+let productName = 'Дроид';
+let pricePerItem = 2000;
+
+// Пиши код ниже этой строки
+
+productName = 'Ремонтный дроид';
+pricePerItem = 3500;
+
+console.log(productName);
+console.log(pricePerItem);
